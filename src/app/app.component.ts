b27405cfcee5b0c8ae2c9bb4b446679a374661de
loicbefve <befve.loic@gmail.com>
2018-12-18 23:23:32 +0100

    Preparation of the application for routing
    Signed-off-by: loicbefve <befve.loic@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,40 +7,8 @@ import {AppareilService} from './services/appareil.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
-  isAuth = false;
-
-  lastUpdate = new Promise(
-    (resolve) => {
-      const date = new Date();
-      setTimeout(
-        () => {
-          resolve(date);
-        }, 2000
-      );
-    }
-  );
-
-  appareils: any[];
-
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    this.appareilService.switchOffAll();
+export class AppComponent {
+  constructor() {
   }
 }
+
